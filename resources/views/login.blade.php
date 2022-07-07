4054169cccee0da598dab5a4b08680204c3cbb43
Cesar Augusto <augustocsar22@gmail.com>
2022-07-07 08:28:45 -0300

    Alteração do title da view de login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <head>
   <meta charset="UTF-8" />
-  <title>Formulário de Login e Registro com HTML5 e CSS3</title>
+  <title>Login</title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
   <link rel="stylesheet" type="text/css" href="./css/style-login.css" />
 </head>
